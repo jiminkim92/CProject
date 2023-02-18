612713f69d03cc78dcaf7eed60ea2d66ce045713
jiminkim92 <wlalsl48@naver.com>
2023-02-18 11:32:24 +0900

    초기 커밋.

---
+++ CProject/CProject.c
@@ -0,0 +1,8 @@
+﻿#include <stdio.h>
+
+int main0() {
+	printf("C언어 수강중!\n");
+	printf("첫번째 수업중!");
+	
+	return 0; 
+}
---
+++ CProject/Print1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	// 주석 : 이 줄은 메모취급
+	/*
+
+	*/
+
+	printf ("작성자 : 김지민\n");
+	printf ("작성날짜 : 2023-02-18\n");
+	printf ("기능 : printf로 문자열 출력");
+
+	return 0;
+}
